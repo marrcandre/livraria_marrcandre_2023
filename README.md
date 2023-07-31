001b85f287fb7ac0e36620f2b936e2e33544195a
Marco André Mendes <marcoandre@gmail.com>
2023-07-31 09:16:16 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Projeto de API REST da LIvraria com Django e DRF
+
+[Marco André](marcoandre@gmail.com)
---
+++ config/urls.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from livraria.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, LivroViewSet
+from usuario.router import router as usuario_router
+
+router = DefaultRouter()
+
+router.register(r"autores", AutorViewSet)
+router.register(r"categorias", CategoriaViewSet)
+router.register(r"editoras", EditoraViewSet)
+router.register(r"livros", LivroViewSet)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include(usuario_router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("", include(router.urls)),
+]
