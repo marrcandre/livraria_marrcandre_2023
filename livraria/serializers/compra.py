b5d6df6f1493c0683bb560bc3123227007586b74
Marco André Mendes <marcoandre@gmail.com>
2023-08-14 15:07:46 -0300

    read_only

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -4,8 +4,8 @@ from livraria.models import Compra
 
 
 class CompraSerializer(ModelSerializer):
-    usuario = CharField(source="usuario.email")
-    status = CharField(source="get_status_display")
+    usuario = CharField(source="usuario.email", read_only=True)
+    status = CharField(source="get_status_display", read_only=True)
 
     class Meta:
         model = Compra
