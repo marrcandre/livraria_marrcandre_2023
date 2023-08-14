e1478ad02bf26ca141f720407e50ad48bf5b8923
Marco André Mendes <marcoandre@gmail.com>
2023-08-14 14:39:59 -0300

    Endpoint para listagem básica de Compras

--- config/urls.py
+++ config/urls.py
@@ -10,7 +10,7 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from livraria.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, LivroViewSet
+from livraria.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, LivroViewSet, CompraViewSet
 from uploader.router import router as uploader_router
 from usuario.router import router as usuario_router
 
@@ -20,6 +20,7 @@ router.register(r"autores", AutorViewSet)
 router.register(r"categorias", CategoriaViewSet)
 router.register(r"editoras", EditoraViewSet)
 router.register(r"livros", LivroViewSet)
+router.register(r"compras", CompraViewSet)
 
 urlpatterns = [
     # OpenAPI 3
---
+++ livraria/views/compra.py
@@ -0,0 +1,10 @@
+from rest_framework.viewsets import ModelViewSet
+
+from livraria.models import Compra
+from livraria.serializers import CompraSerializer
+
+
+class CompraViewSet(ModelViewSet):
+    queryset = Compra.objects.all()
+    serializer_class = CompraSerializer
+
