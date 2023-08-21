6d09f1457e1c09ca36b5269dd14039438b013896
Marco André Mendes <marcoandre@gmail.com>
2023-08-21 16:09:16 -0300

    inclusão do telefone no autor

--- livraria/models/autor.py
+++ livraria/models/autor.py
@@ -4,6 +4,7 @@ from django.db import models
 class Autor(models.Model):
     nome = models.CharField(max_length=255)
     email = models.EmailField(null=True, blank=True)
+    telefone = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return self.nome
