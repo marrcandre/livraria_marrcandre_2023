ee89b68414ecadc974eb51a3a11d8ce5e0143094
Marco André Mendes <marcoandre@gmail.com>
2023-08-24 13:20:16 -0300

    tirado valor default do preco do item

--- livraria/models/compra.py
+++ livraria/models/compra.py
@@ -34,10 +34,13 @@ class Compra(models.Model):
         # return total
         return sum(item.livro.preco * item.quantidade for item in self.itens.all())
 
+    def __str__(self):
+        return f"Compra {self.id} - {self.usuario.email}"
+
 
 class ItensCompra(models.Model):
     compra = models.ForeignKey(Compra, on_delete=models.CASCADE, related_name="itens")
     livro = models.ForeignKey(Livro, on_delete=models.PROTECT, related_name="+")
     quantidade = models.IntegerField(default=1)
-    preco_item = models.DecimalField(max_digits=10, decimal_places=2, default=1)
+    preco_item = models.DecimalField(max_digits=10, decimal_places=2)
 
