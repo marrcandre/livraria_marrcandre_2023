4cfcc090d1261f310278e7b8621a26c23cc72cbe
Marco André Mendes <marcoandre@gmail.com>
2023-09-25 14:48:59 -0300

    scripts de pre execução

--- config/settings.py
+++ config/settings.py
@@ -114,18 +114,8 @@ if MODE in ["PRODUCTION", "MIGRATE"]:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     MEDIA_URL = '/media/' 
 else:    
-    from subprocess import run, PIPE
-    COMMAND = "nmcli device show | grep IP4.ADDRESS | head -1 | awk '{print $2}' | rev | cut -c 4- | rev"
-    result = run(
-        COMMAND,
-        check=False,
-        stdout=PIPE,
-        stderr=PIPE,
-        universal_newlines=True,
-        shell=True
-    )
-    ip_addr = result.stdout.strip()
-    MEDIA_URL = f"http://{ip_addr}:19003/media/"
+    MY_IP = os.getenv("MY_IP", "127.0.0.1")
+    MEDIA_URL = f"http://{MY_IP}:19003/media/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
